7f1ef2b4e21210c672b4651bcc546c02d3010357
romapres2010 <50259507+romapres2010@users.noreply.github.com>
2020-01-31 18:34:03 +0300

    0.2.21

--- Neighborhoods in Moscow analyze.py
+++ Neighborhoods in Moscow analyze.py
@@ -283,28 +283,118 @@ Moscow_map
 
 
 
+###############################################################################
+###############################################################################
+# Analyze Neighborhood
+###############################################################################
+###############################################################################
 
-
-
-
-
-
-
-
+# list of the recomended  Boughs from cluster 1
 Moscow_Recomended_Borough_list = Moscow_Borough_df[Moscow_Borough_df['Cluster_Labels'].isin(['1'])]['Borough_Name']
 
+# list of all subcategories Gym / Fitness Center
+gym_categories = ['Gym / Fitness Center','Boxing Gym','Climbing Gym','Cycle Studio','Gymnastics Gym','Gym','Martial Arts Dojo','Outdoor Gym','Pilates Studio','Weight Loss Center','Yoga Studio']
 
-Moscow_Borough_df.to_csv("data\Moscow_Borough_Clustering_df.csv", index = False)
+# Make venues subset of all subcategories of "Gym / Fitness Center"
+Moscow_gym_venues_df = Moscow_venues_df[Moscow_venues_df['Venue_Category_Name'].isin(gym_categories)]
+print('There are {} venues of all "Gym / Fitness Center" subcategories in Moscow City'.format(Moscow_gym_venues_df.shape[0]))
 
-
-
-
-# Delete Venues that placed outside our cluster 
+# Delete Venues that placed outside our cluster 1
 Moscow_gym_venues_df = Moscow_gym_venues_df[Moscow_gym_venues_df['Borough_Name'].isin(Moscow_Recomended_Borough_list)]
+print('There are {} venues of all "Gym / Fitness Center" subcategories in 1 Cluster'.format(Moscow_gym_venues_df.shape[0]))
+
+# save the dataset
 Moscow_gym_venues_df.to_csv("data\Moscow_gym_venues_df.csv", index = False)
 
 
-print(Moscow_Clustering_df)
+###############################################################################
+# Visialize a Heatmap of Gym in 1 cluster
+###############################################################################
+# Moscow latitude and longitude values
+Moscow_lat= 55.7504461
+Moscow_lng= 37.6174943
+
+# create map
+Moscow_map = folium.Map(location=[Moscow_lat, Moscow_lng], zoom_start=11)
+
+# generate choropleth map
+Moscow_map.choropleth(
+    geo_data=mo_geojson,
+    data=Moscow_Borough_df,
+    name='Population Density',
+    columns=['Borough_Name', 'Cluster_Labels'],
+    key_on='feature.properties.NAME',
+    fill_color='YlOrRd', 
+    fill_opacity=0.4, 
+    line_opacity=0.2,
+    legend_name='Borough Gym Clustering in Moscow City')
+
+# List comprehension to make out list of lists
+heat_data = [[row['Venue_Latitude'], row['Venue_Longitude']] for index, row in Moscow_gym_venues_df.iterrows()]
+
+# Add HeatMap
+HeatMap(heat_data).add_to(Moscow_map)
+#folium.GeoJson(mo_geojson).add_to(Moscow_map)
+
+# display map
+Moscow_map
+
+
+
+
+#==============================================================================
+# Add markers to map for borough 
+#==============================================================================
+mo_geojson = 'data\mo.geojson'
+
+# Moscow latitude and longitude values
+Moscow_lat= 55.7504461
+Moscow_lng= 37.6174943
+
+# create map 
+Moscow_map = folium.Map(location=[Moscow_lat, Moscow_lng], zoom_start=10)
+
+# generate choropleth map
+Moscow_map.choropleth(
+    geo_data=mo_geojson,
+    data=Moscow_Borough_df,
+    name='Population Density',
+    columns=['Borough_Name', 'Cluster_Labels'],
+    key_on='feature.properties.NAME',
+    fill_color='YlOrRd', 
+    fill_opacity=0.7, 
+    line_opacity=0.2,
+    legend_name='Borough Gym Clustering in Moscow City')
+
+
+# Add Borougs center as markers to Moscow map 
+for Venue_name, lat, lng in zip(Moscow_gym_venues_df['Venue_Name'], Moscow_gym_venues_df['Venue_Latitude'], Moscow_gym_venues_df['Venue_Longitude']):
+    folium.features.CircleMarker(
+        [lat, lng],
+        radius=5, # define how big you want the circle markers to be
+        color='yellow',
+        fill=True,
+        #popup=folium.Popup('{}'.format(Venue_name), parse_html=True),
+        fill_color='blue',
+        fill_opacity=0.6
+    ).add_to(Moscow_map)
+
+    folium.Circle([lat, lng], radius=250, color='blue', fill=False).add_to(Moscow_map)
+
+Moscow_map
+
+# display map
+
+
+
+
+
+
+
+
+
+
+
 
 
 
